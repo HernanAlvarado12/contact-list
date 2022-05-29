da4e16ff005ff2ab14a4092a1b030c5d9f047744
HernanAlvarado12 <hernanvid123@gmail.com>
2022-05-29 13:09:59 -0500

    Solution 3

--- contact.js
+++ contact.js
@@ -1,14 +1,14 @@
 let contacts = [];
 
-function add(name, lastname, telefono, cuidad, direccion){
+function add(name, lastname, phone, city, direction){
     contacts.push({
         "id": contacts.length +1,
         "name": name,
         "lastname": lastname,
-        "phone": telefono,
+        "phone": phone,
         "location": {
-            "cit": cuidad,
-            "direction": direccion
+            "cit": city,
+            "direction": direction
         }
     });
 }
@@ -24,6 +24,26 @@ function deleteContact(name){
     contacts = current;
 }
 
+let search = function(name){
+    let object = null;
+    for(let i = 0; i < contacts.length; i++){
+        if(contacts[i].name === name){
+            object = contacts[i];
+        }
+    }
+    return object;
+}
+
+function updateContact(name, lastname, cell){
+    let object = search(name);
+    if(object != null){
+        object.lastname = lastname;
+        object.phone = cell;
+    }
+}
+
+
+
 
 function toString(){
     let string = "";
@@ -36,6 +56,10 @@ function toString(){
 
 add("Hernan", "Alvarado", 123, "Tasco", "Calle 6 #5-48");
 add("David", "Parra", 456, "Bogota", "Carrera 7b #138-68");
-contacts.forEach(x => console.log(x));
 
+contacts.forEach(x => console.log(x));
+console.log("Se ha modificado un contacto dentro de nuestra agenda");
+
+updateContact("Hernan", "Alvarado Parra", 3138075404);
+contacts.forEach(x => console.log(x));
 
